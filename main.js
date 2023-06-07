ceaa113adbece47d1d73aac668159a26aaa517d7
Matthew Oshimo <moshimo90696@gmail.com>
2023-06-07 04:08:56 -0700

    allow for modifying value shown

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 let value = 0
-
+let amount = 1
 function tester(){
-    for (var i = 0; i < 10; i++){
-    value += i
-    }
+    value += amount
     return(value)
+}
+function amounter(){
+    return amount
+}
+function add(){
+    amount += 1
+    return amount
+}
+function subtract(){
+    amount -= 1
+    return amount
 }
