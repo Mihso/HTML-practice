ff40ffda8e820ac5033aeaba964b40e3ef3ad71a
Matthew Oshimo <moshimo90696@gmail.com>
2023-06-30 01:23:49 -0700

    create table that stores previous problems

--- main.js
+++ main.js
@@ -24,8 +24,13 @@ function tester(){
         return(0)
     }
     data.push(amount);
-    var li = document.createElement('li');
-    li.innerText = amount;
+    var li = document.createElement('tr');
+    var col1 = document.createElement('td');
+    var col2 = document.createElement('td');
+    col1.innerHTML = document.getElementById("number1").value + value + document.getElementById("number2").value;
+    col2.innerHTML = amount;
+    li.appendChild(col1);
+    li.appendChild(col2);
     record.appendChild(li);
     return amount;
 }
